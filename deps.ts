1982afa0ca0b525801b6d29c3448ee8953496bfc
João Cerqueira <oss@cerqueira.io>
2021-04-05 18:51:19 +0100

    style: code formatted

--- deps.ts
+++ deps.ts
@@ -2,5 +2,5 @@
 export { parse as parseArgs } from "https://deno.land/std@0.91.0/flags/mod.ts";
 
 // x/mysql
-export { Client as MySQLClient } from "https://deno.land/x/mysql/mod.ts"
-export type { ClientConfig as MySQLClientConfig } from "https://deno.land/x/mysql/mod.ts"
+export { Client as MySQLClient } from "https://deno.land/x/mysql/mod.ts";
+export type { ClientConfig as MySQLClientConfig } from "https://deno.land/x/mysql/mod.ts";
